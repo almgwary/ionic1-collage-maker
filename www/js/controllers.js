af690725ebe67bccd9415f40ec619cc7f3f9096c
almgwary <almgwary@outlook.com>
2017-05-26 08:42:32 +0200

    create collage  maker code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,94 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$cordovaCamera) {
+.controller('DashCtrl', function($scope,$cordovaCamera,$ionicPopup) {
   $scope.title = "Welcome to Ionic";
 
    var options = {};
 
 
-  $scope.onClick = function () {
+  $scope.getImage = function (x,y,w) {
     console.log('clicked');
     console.log('start');
     $cordovaCamera.getPicture(options).then(function(imageURI) {
       console.log('then',imageURI);
-      $scope.imageURI = imageURI ; 
+      $scope.imageURI = imageURI ;
+      $scope.drawImage(imageURI,x,y,w); 
     }, function(err) {
       // error
       console.log('error',err);
     });
-    console.log('done',err);
+    console.log('done');
   }
+  
+
+    // canvas start here
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    // setup
+    canvas.width = 600;
+    canvas.height = 400;
+    context.globalAlpha = 1.0;
+    context.beginPath();
+
+    $scope.drawImage = function (image_url,x,y,w) {
+      
+      var img = new Image();
+      img.onload = function() {
+          console.log('draw image');
+          context.drawImage(img, x, y,w, w * img.height / img.width);
+      };
+      img.src = image_url;
+
+    }
+
+
+    $scope.save = function () {
+      window.canvas2ImagePlugin.saveImageDataToLibrary(
+        function(msg){
+            console.log(msg);
+             var alertPopup = $ionicPopup.alert({
+            title: 'Saved!',
+            template: 'check gallary'
+            });
+            alertPopup.then(function(res) {
+                console.log('done');
+            });
+        },
+        function(err){
+            console.log(err);
+             var alertPopup = $ionicPopup.alert({
+            title: 'Not Saved!',
+            template: 'check console'
+            });
+            alertPopup.then(function(res) {
+                console.log('done');
+            });
+        },
+          canvas
+      );
+    }
+
+    $scope.get1stImage = function () {
+      $scope.getImage( 0, 0,200);
+    }
+
+
+    $scope.get2ndtImage = function () {
+      $scope.getImage( 0, 205,200);
+    }
+
+    $scope.get4thtImage = function () {
+      $scope.getImage( 205, 205,200);
+    }
+
+    $scope.get3rdtImage = function () {
+      $scope.getImage(205, 0,200);
+    }
+
+    
+    
+
+  // canvas end here
 
   
 
